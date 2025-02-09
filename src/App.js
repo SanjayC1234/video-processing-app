c5236928e7eb7249044171771bcaba41b5cf16ec
SanjayC1234 <sanjaychauhann2023@gmail.com>
2025-02-09 22:44:27 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Video Processing App</h1>
+      <p>Welcome to our AI-powered video processing tool!</p>
+    </div>
+  );
+}
+
+export default App;
